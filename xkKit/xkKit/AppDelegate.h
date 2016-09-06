0af55f9533b16f5af1c15df04c44667798e29345
zhengkai <manager85695157@163.com>
2016-09-06 16:43:52 +0800

    初始化项目

---
+++ xkKit/xkKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xkKit
+//
+//  Created by timetang-mac-01 on 16/9/6.
+//  Copyright © 2016年 kyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xkKit/xkKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xkKit
+//
+//  Created by timetang-mac-01 on 16/9/6.
+//  Copyright © 2016年 kyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
