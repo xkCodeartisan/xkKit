8b97ef173cf216f17d5ba73858561b56f531c5d8
zhengkai <manager85695157@163.com>
2016-09-06 16:45:11 +0800

    创建了dog

---
+++ xkKit/xkKit/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  xkKit
+//
+//  Created by timetang-mac-01 on 16/9/6.
+//  Copyright © 2016年 kyle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ xkKit/xkKit/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  xkKit
+//
+//  Created by timetang-mac-01 on 16/9/6.
+//  Copyright © 2016年 kyle. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
